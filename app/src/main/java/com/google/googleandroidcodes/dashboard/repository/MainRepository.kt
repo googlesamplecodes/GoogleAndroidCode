7e85cf271f470dfaa71b4461c472821182544231
Origin <origin1234527@gmail.com>
2021-02-16 14:51:29 +0530

    New Updates

--- app/src/main/java/com/google/googleandroidcodes/dashboard/repository/MainRepository.kt
+++ app/src/main/java/com/google/googleandroidcodes/dashboard/repository/MainRepository.kt
@@ -8,25 +8,36 @@ import com.google.llc.retrofit.RetrofitClient
 import com.google.llc.utils.NetworkResult
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import java.lang.Error
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(context: Context, var newsApi: NewsApi) {
+    var compositeDisposable: CompositeDisposable
 
+    init {
+        compositeDisposable = CompositeDisposable()
+    }
 
     fun getNews(): MutableLiveData<NetworkResult<TopHeadLineResponse>> {
         val mutableNewslist = MutableLiveData<NetworkResult<TopHeadLineResponse>>()
-        newsApi.getNews().subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                mutableNewslist.postValue(NetworkResult.Success(it))
-            }, {
-                it?.message?.let {
-                    mutableNewslist.postValue((NetworkResult.Error(it)))
-                }
+        compositeDisposable.add(
+            newsApi.getNews().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    mutableNewslist.postValue(NetworkResult.Success(it))
+                }, {
+                    it?.message?.let {
+                        mutableNewslist.postValue((NetworkResult.Error(it)))
+                    }
 
-            })
+                })
+        )
         return mutableNewslist
+    }
+
+    fun clear() {
+        compositeDisposable.dispose()
     }
 }
--- app/src/main/java/com/google/googleandroidcodes/dashboard/viewmodels/MainViewModels.kt
+++ app/src/main/java/com/google/googleandroidcodes/dashboard/viewmodels/MainViewModels.kt
@@ -21,4 +21,8 @@ class MainViewModels @Inject constructor(
         return mainRepository.getNews()
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        mainRepository.clear()
+    }
 }
